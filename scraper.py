7163de6d5ec068374df463c95d6e1d416b2a6697
Jorge González <32584615+jgusal@users.noreply.github.com>
2020-10-23 00:40:37 +0100

    Update scraper.py
    Store timestamp inside file

--- scraper.py
+++ scraper.py
@@ -1,6 +1,7 @@
 from time import sleep
 import requests
 import datetime
+import json
 
 # Parameters
 URL = 'https://api.jcdecaux.com/vls/v1/stations?apiKey=a9e2b6162328756a94ef2d8af7e6923efdc0b457'
@@ -13,8 +14,9 @@ while True:
         request = requests.get(URL)
         request.raise_for_status()
         with open('./json/%s.json' % timestamp, 'w') as out:
-            out.write(request.text)
-    except requests.exceptions.RequestException as ex:
+            data = {"timestamp": timestamp, "payload": request.json()}
+            out.write(json.dumps(data))
+    except Exception as ex:
         with open('./error/%s' % timestamp, 'w') as out:
             out.write(str(ex))
             
