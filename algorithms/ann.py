8d01d70ff0e025b9d9582d1531568cf597de0671
joshuacassidy <joshcassidy79@gmail.com>
2020-12-21 16:54:52 +0000

    update ann

--- algorithms/ann.py
+++ algorithms/ann.py
@@ -1,7 +1,25 @@
+import os
+os.environ['PYTHONHASHSEED']=str(11)
+import matplotlib
+matplotlib.use("TkAgg")
+import random
+random.seed(11)
+import numpy as np
+np.random.seed(11)
+import tensorflow as tf
+tf.random.set_random_seed(11)
+
+from sklearn.metrics import confusion_matrix, classification_report, accuracy_score
+from sklearn.utils import shuffle
+import matplotlib.pyplot as plt
+plt.rc('font', size=18)
+plt.rcParams['figure.constrained_layout.use'] = True
+import sys
+
+
+
 import statistics
 from sklearn.preprocessing import LabelEncoder
-
-labels_index = 19
 
 import random
 import json
@@ -20,13 +38,11 @@ from keras.optimizers import RMSprop
 from keras.optimizers import SGD
 import keras
 import pandas as pd
-from hyperas.distributions import uniform
-from hyperas import optim
-from hyperas.distributions import choice, uniform
-from hyperopt import Trials, STATUS_OK, tpe
 import tensorflow as tf
 from keras.utils import to_categorical
 from keras.utils import plot_model
+
+
 
 
 
@@ -42,44 +58,207 @@ def data():
     data = pd.read_csv("data/transform_data/merged_data.csv", header=None)
     Label_enc = LabelEncoder()
     
-    data[0] = stats.zscore([datetime.datetime.strptime(i, '%Y-%m-%d_%H-%M-%S').timestamp() for i in data[0].values])
-    data[1] = Label_enc.fit_transform(data[1].values)
-    data[2] = stats.zscore((data.loc[:,2]))
-    data[3] = stats.zscore((data.loc[:,3]))
-    data[4] = stats.zscore((data.loc[:,4]))
-    data[5] = stats.zscore((data.loc[:,5]))
-    data[6] = stats.zscore((data.loc[:,6]))
-    data[7] = stats.zscore((data.loc[:,7]))
-    data[8] = stats.zscore((data.loc[:,8]))
-    data[9] = stats.zscore((data.loc[:,9]))
-    data[10] = Label_enc.fit_transform(data[10].values)
-    data[11] = stats.zscore((data.loc[:,11]))
-    data[12] = stats.zscore((data.loc[:,12]))
-    data[13] = Label_enc.fit_transform(data[13].values)
+    training_data = np.zeros(shape=(462000, 211))
+
+
+    training_data[:, 0] = stats.zscore([datetime.datetime.strptime(i, '%Y-%m-%d_%H-%M-%S').timestamp() for i in data[0].values])
+    training_data[:, 1] = stats.zscore((data.loc[:,2]))
+    training_data[:, 2] = stats.zscore((data.loc[:,3]))
     
-    data[14] = Label_enc.fit_transform(data[14].values)
-    data[15] = Label_enc.fit_transform(data[15].values)
-    data[16] = Label_enc.fit_transform(data[16].values)
-
-    data[17] = stats.zscore((data.loc[:,17]))
-    data[18] = stats.zscore((data.loc[:,18]))
-    data[19] = stats.zscore((data.loc[:,19]))
+    training_data[:, 3] = stats.zscore((data.loc[:,4]))
+    training_data[:, 4] = stats.zscore((data.loc[:,5]))
+    training_data[:, 5] = stats.zscore((data.loc[:,6]))
+    training_data[:, 6] = stats.zscore((data.loc[:,7]))
     
-    X_train, X_test, y_train, y_test = train_test_split(data.iloc[:,:18], data.iloc[:,19], test_size=0.25, random_state=42)
-    return X_train, y_train, X_test, y_test
+    training_data[:, 7] = stats.zscore((data.loc[:,8]))
+    training_data[:, 8] = stats.zscore((data.loc[:,9]))
+    training_data[:, 9:24] = to_categorical(Label_enc.fit_transform(data[10].values))
+    training_data[:, 24] = stats.zscore((data.loc[:,11]))
+    
+    training_data[:, 25] = stats.zscore((data.loc[:,12]))
+    training_data[:, 26:136] = to_categorical(Label_enc.fit_transform(data[13].values))
+    training_data[:, 136:138] = to_categorical(data[14].values)
+    training_data[:, 139] = to_categorical(Label_enc.fit_transform(data[15].values)).flatten()
+
+    training_data[:, 140:142] = to_categorical(Label_enc.fit_transform(data[16].values))
+    training_data[:, 142] = stats.zscore((data.loc[:,17]))
+    training_data[:, 143] = stats.zscore((data.loc[:,18]))
+    print("loading")
+    
+    training_data[:, 144:175] = to_categorical(Label_enc.fit_transform(
+            [datetime.datetime.strptime(i, '%Y-%m-%d_%H-%M-%S').day for i in data[0].values]
+    ))
+    
+
+    training_data[:, 175:177] = to_categorical(Label_enc.fit_transform(
+        [datetime.datetime.strptime(i, '%Y-%m-%d_%H-%M-%S').weekday() < 5 for i in data[0].values]
+    ))
+    
+
+    training_data[:, 177:180] = to_categorical(Label_enc.fit_transform(
+        [datetime.datetime.strptime(i, '%Y-%m-%d_%H-%M-%S').month for i in data[0].values]
+    ))
+    
+
+    training_data[:, 180:187] = to_categorical(Label_enc.fit_transform(
+        [datetime.datetime.strptime(i, '%Y-%m-%d_%H-%M-%S').weekday() for i in data[0].values]
+    ))
+
+    training_data[:, 187:211] = to_categorical(Label_enc.fit_transform(
+        [datetime.datetime.strptime(i, '%Y-%m-%d_%H-%M-%S').hour for i in data[0].values]
+    ))
+
+    labels = data.loc[:,19]
+    print("loaded")
+    return training_data, labels
+
+    
+def root_mean_squared_error(y_true, y_pred):
+    return keras.backend.sqrt(keras.backend.mean(keras.backend.square(y_pred - y_true))) 
 
 
-X_train, y_train, X_test, y_test = data()
 
-model = Sequential()
-model.add(Dense(16, input_shape=(18,), activation='relu'))
-model.add(Dropout(0.5))
+training_data, labels = data()
 
-model.add(Dense(1))
+def model_1():
+    model = Sequential()
+    model.add(Dense(28, input_shape=(211,), activation='relu'))
+    model.add(Dense(28, activation='relu'))
 
-model.compile(optimizer = RMSprop(), loss='mse', metrics=['mae'])
+    model.add(Dense(1))
 
-model.fit(X_train, y_train, batch_size=5, epochs=30, callbacks=[keras.callbacks.EarlyStopping(monitor='acc', mode='max', patience=2, restore_best_weights=True)])
+    model.compile(optimizer = Adadelta(), loss=root_mean_squared_error, metrics=['mae'])
+    return model
+
+def model_2():
+    model = Sequential()
+    model.add(Dense(28, input_shape=(211,), activation='relu'))
+    model.add(Dense(28, activation='relu'))
+    model.add(Dropout(0.5))
+
+    model.add(Dense(1))
+
+    model.compile(optimizer = Adadelta(), loss=root_mean_squared_error, metrics=['mae'])
+    return model
+
+
+def model_3():
+    model = Sequential()
+    model.add(Dense(32, input_shape=(211,), activation='relu'))
+    model.add(Dense(32, activation='relu'))
+    # model.add(Dense(16, activation='relu'))
+    # model.add(Dropout(0.5))
+
+    model.add(Dense(1))
+
+    model.compile(optimizer = Adadelta(), loss=root_mean_squared_error, metrics=['mae'])
+    return model
+
+def model_4():
+    model = Sequential()
+    model.add(Dense(64, input_shape=(211,), activation='relu'))
+    model.add(Dense(64, activation='relu'))
+
+    model.add(Dense(1))
+
+    model.compile(optimizer = Adadelta(), loss=root_mean_squared_error, metrics=['mae'])
+    return model
+
+def model_5():
+    model = Sequential()
+    model.add(Dense(32, input_shape=(211,), activation='relu'))
+    model.add(Dense(32, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(32, activation='relu'))
+    model.add(Dropout(0.5))
+
+    model.add(Dense(1))
+
+    model.compile(optimizer = Adadelta(), loss=root_mean_squared_error, metrics=['mae'])
+    return model
+
+
+
+from sklearn.model_selection import KFold
+
+models = [model_1, model_2, model_3, model_4, model_5]
+
+best_model = None
+best_rmse = None
+best_results = None
+
+for m in models:
+
+    ann_results = []
+    kf = KFold(n_splits=5, shuffle=True, random_state=20300057)
+    for train_index, test_index in kf.split(training_data):
+        model = m()
+        x_train_data_fold = training_data[train_index[:], :]
+        y_train_data_fold = labels.iloc[train_index[:]]
+        x_test_data_fold = training_data[test_index, :]
+        y_test_data_fold = labels.iloc[test_index]
+
+        history = model.fit(
+            x_train_data_fold, 
+            y_train_data_fold, 
+            batch_size=30, 
+            epochs=5, 
+            validation_split=0.1, 
+            callbacks=[keras.callbacks.EarlyStopping(monitor='mae', mode='max', patience=2, restore_best_weights=True)])
+
+        results = model.evaluate(x_test_data_fold, y_test_data_fold)
+        ann_results.append(results)
+        print(ann_results)
+
+    mean_rmse = [i[0] for i in ann_results]
+    mean_mae = [i[1] for i in ann_results]
+
+    if best_model is None or mean_rmse < best_rmse:
+        best_model = m
+        best_rmse = mean_rmse
+        best_results = ann_results
+
+
+X_train, X_test, y_train, y_test = train_test_split(training_data, labels, test_size=0.25, random_state=11)
+
+best_mean_rmse = [i[0] for i in best_results]
+best_mean_mae = [i[1] for i in best_results]
+
+print("average mean rmse", best_mean_rmse)
+print("average mean mae", best_mean_mae)
+
+model = best_model()
+
+model.summary()
+
+history = model.fit(
+    X_train, 
+    y_train, 
+    batch_size=30, 
+    epochs=20, 
+    validation_split=0.1
+    )
+
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'val'], loc='upper left')
+plt.show()
+plt.clf()
+
+
+plt.plot(history.history['mean_absolute_error'])
+plt.plot(history.history['val_mean_absolute_error'])
+plt.title('model mean_absolute_error')
+plt.ylabel('mean_absolute_error')
+plt.xlabel('epoch')
+plt.legend(['train', 'val'], loc='upper left')
+plt.show()
+plt.clf()
+
+
 results = model.evaluate(X_test, y_test)
 print(results)
 
