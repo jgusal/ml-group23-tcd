ae81ee0f719f8ebdfb536f24f4b6e402dd7ee16b
Jorge González <32584615+jgusal@users.noreply.github.com>
2020-12-25 00:00:54 +0100

    Update "Decision Tree" and "kNN Regressor", README

--- algorithms/Decision Trees.py
+++ algorithms/Decision Trees.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[13]:
-
-
 from sklearn.preprocessing import LabelEncoder
 import pandas as pd
 import numpy as np
@@ -12,136 +6,114 @@ import datetime
 from keras.utils import to_categorical
 from sklearn.tree import DecisionTreeRegressor 
 from sklearn.model_selection import train_test_split
+import time
+from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
+import matplotlib.pyplot as plt
+from sklearn import tree
+from scipy import stats
 
 
-# In[2]:
+## Encode data
+def get_data(csv):
+    data = pd.read_csv(csv, header=None)
+
+    data[0] = stats.zscore([datetime.datetime.strptime(i, '%Y-%m-%d_%H-%M-%S').timestamp() for i in data[0].values])
+    data[2] = stats.zscore((data.loc[:,2]))
+    data[3] = stats.zscore((data.loc[:,3]))
+    data[4] = stats.zscore((data.loc[:,4]))
+    data[5] = stats.zscore((data.loc[:,5]))
+    data[6] = stats.zscore((data.loc[:,6]))
+    data[7] = stats.zscore((data.loc[:,7]))
+    data[8] = stats.zscore((data.loc[:,8]))
+    data[9] = stats.zscore((data.loc[:,9]))
+    data[11] = stats.zscore((data.loc[:,11]))
+    data[12] = stats.zscore((data.loc[:,12]))
+    data[17] = stats.zscore((data.loc[:,17]))
+    data[18] = stats.zscore((data.loc[:,18]))
+    #data[19] = stats.zscore((data.loc[:,19]))
+
+    data.columns= ['timestamp', 'city', 'av_temp', 'min_temp', 'max_temp', 'pressure',
+                   'humidity', 'visibility', 'wind_speed', 'clouds', 'weather_desc',
+                   'sunrise', 'sundown', 'bike_station_address', 'banking', 'bonus',
+                   'status', 'total_bike_stands', 'available_bike_stands', 'available_bikes']
+
+    dummy = pd.get_dummies(data, prefix=['city', 'weather_desc', 'bike_station_address', 'banking', 'bonus', 'status'],
+                           columns=['city', 'weather_desc', 'bike_station_address', 'banking', 'bonus', 'status'])
+
+    final = dummy.drop(['banking_False', 'status_False', 'available_bike_stands'], axis=1)
+    final = final.fillna(final.mean())
+
+    return final
 
 
-def data():
-    data = pd.read_csv("merged_data.csv", header=None)
-    Label_enc = LabelEncoder()
-    
-    training_data = np.zeros(shape=(462000, 210))
-    
-    
-    training_data[:, 0] = stats.zscore([datetime.datetime.strptime(i, '%Y-%m-%d_%H-%M-%S').timestamp() for i in data[0].values])
-    training_data[:, 1] = stats.zscore((data.loc[:,2]))
-    training_data[:, 2] = stats.zscore((data.loc[:,3]))
-    
-    training_data[:, 3] = stats.zscore((data.loc[:,4]))
-    training_data[:, 4] = stats.zscore((data.loc[:,5]))
-    training_data[:, 5] = stats.zscore((data.loc[:,6]))
-    training_data[:, 6] = stats.zscore((data.loc[:,7]))
-    
-    training_data[:, 7] = stats.zscore((data.loc[:,8]))
-    training_data[:, 8] = stats.zscore((data.loc[:,9]))
-    training_data[:, 9:24] = to_categorical(Label_enc.fit_transform(data[10].values))
-    training_data[:, 24] = stats.zscore((data.loc[:,11]))
-    
-    training_data[:, 25] = stats.zscore((data.loc[:,12]))
-    training_data[:, 26:136] = to_categorical(Label_enc.fit_transform(data[13].values))
-    training_data[:, 136:138] = to_categorical(data[14].values)
-    training_data[:, 139] = to_categorical(Label_enc.fit_transform(data[15].values)).flatten()
-    
-    training_data[:, 140:142] = to_categorical(Label_enc.fit_transform(data[16].values))
-    training_data[:, 142] = stats.zscore((data.loc[:,17]))
+## Split data
+def split_data(full_data):
 
-    print("loading")
-    
-    training_data[:, 143:174] = to_categorical(Label_enc.fit_transform(
-            [datetime.datetime.strptime(i, '%Y-%m-%d_%H-%M-%S').day for i in data[0].values]
-    ))
-    
-    
-    training_data[:, 174:176] = to_categorical(Label_enc.fit_transform(
-        [datetime.datetime.strptime(i, '%Y-%m-%d_%H-%M-%S').weekday() < 5 for i in data[0].values]
-    ))
-    
-    
-    training_data[:, 176:179] = to_categorical(Label_enc.fit_transform(
-        [datetime.datetime.strptime(i, '%Y-%m-%d_%H-%M-%S').month for i in data[0].values]
-    ))
-    
-    
-    training_data[:, 179:186] = to_categorical(Label_enc.fit_transform(
-        [datetime.datetime.strptime(i, '%Y-%m-%d_%H-%M-%S').weekday() for i in data[0].values]
-    ))
-    
-    training_data[:, 186:210] = to_categorical(Label_enc.fit_transform(
-        [datetime.datetime.strptime(i, '%Y-%m-%d_%H-%M-%S').hour for i in data[0].values]
-    ))
-    
-    labels = data.loc[:,19]
-    print("loaded")
-    return training_data, labels
+    # Split data into historic and future data to evaluate our future predictions
+    train_size = int(len(full_data) * 0.87)
+    historic, future = full_data[0:train_size], full_data[train_size:len(full_data)]
+
+    historic_target = historic.available_bikes
+    future_real_target = future.available_bikes
+
+    historic_train = historic.drop(['available_bikes'], axis=1)
+    future_test = future.drop(['available_bikes'], axis=1)
+
+    return historic_train, historic_target, future_test, future_real_target
+
+# Read data
+csv = "../data/transformed_data/merged_data.csv"
+data = get_data(csv)
+historic_train, historic_target, future_test, future_real_target = split_data(data)
+X_train, y_train, X_test, y_test = (historic_train, historic_target, future_test, future_real_target)
 
 
-# In[7]:
-
-
-training_data, labels = data()
-
-
-# In[14]:
-
-
-X_train, X_test, y_train, y_test = train_test_split(training_data, labels, test_size=0.25, random_state=11)
-
-
-# In[10]:
-
-
+# Final model
 regressor = DecisionTreeRegressor(random_state=0) 
 
-
-# In[15]:
-
-
+start = time.time()
 regressor.fit(X_train, y_train)
+end = time.time()
 
+print("Training took %f seconds" % (end - start))
 
-# In[16]:
-
-
+# Predict test data
 prediction = regressor.predict(X_test)
 
+## MAE for training data
+train_mae = mean_absolute_error(y_train, regressor.predict(X_train))
+print("Training data MAE: ", train_mae, "\n")
 
-# In[17]:
+## MAE for test data
+test_mae = mean_absolute_error(y_test, prediction)
+print("Test data MAE: ", test_mae, "\n")
 
+## RMSE
+test_mse = mean_squared_error(y_test, prediction)
+test_rmse = np.sqrt(test_mse)
+print("Test data MSE: ", test_mse)
+print("Test data RMSE: ", test_rmse, "\n")
 
-corr = (y_test.values == prediction)
-t = [e for e in corr if e]
-len(t) / len(corr) * 100
+## R-Squared
+test_r2 = r2_score(y_test, prediction)
+print("Test data R2: ", test_r2, "\n")
 
-
-# In[18]:
-
-
-import matplotlib.pyplot as plt
-
-
-# In[26]:
-
-
-plt.plot(y_test.values[500: 600])
-plt.plot(prediction[500: 600])
+# Plot first levels of the decision tree
+tree.plot_tree(regressor, max_depth = 2)
 plt.show()
 
+## Statistics
+kstest_results_actual_values = stats.kstest(future_real_target, 'norm')
+print("kstest_results_actual_values", kstest_results_actual_values)
+kstest_results_predicted_values = stats.kstest(prediction, 'norm')
+print("kstest_results_predicted_values", kstest_results_predicted_values)
 
-# In[23]:
+P_VALUE = 0.05
 
-
-from sklearn.metrics import mean_squared_error
-
-
-# In[25]:
-
-
-mean_squared_error(y_test.values, prediction)
-
-
-# In[ ]:
-
-
-
-
+if kstest_results_predicted_values.pvalue <= P_VALUE or kstest_results_predicted_values.pvalue <= P_VALUE:
+    print(stats.kruskal(future_real_target, prediction))
+    print(stats.mannwhitneyu(future_real_target, prediction))
+else:
+    levene_results = stats.levene(future_real_target, prediction)
+    print("levene_results", levene_results)
+    print(stats.ttest_ind(future_real_target, prediction, equal_var= levene_results.pvalue > P_VALUE))
--- algorithms/kNN Regressor.py
+++ algorithms/kNN Regressor.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[4]:
-
-
 from sklearn.preprocessing import LabelEncoder
 import pandas as pd
 import numpy as np
@@ -12,142 +6,127 @@ import datetime
 from keras.utils import to_categorical
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.model_selection import train_test_split
-
-
-# In[2]:
-
-
-def data():
-    data = pd.read_csv("merged_data.csv", header=None)
-    Label_enc = LabelEncoder()
-    
-    training_data = np.zeros(shape=(462000, 210))
-    
-    
-    training_data[:, 0] = stats.zscore([datetime.datetime.strptime(i, '%Y-%m-%d_%H-%M-%S').timestamp() for i in data[0].values])
-    training_data[:, 1] = stats.zscore((data.loc[:,2]))
-    training_data[:, 2] = stats.zscore((data.loc[:,3]))
-    
-    training_data[:, 3] = stats.zscore((data.loc[:,4]))
-    training_data[:, 4] = stats.zscore((data.loc[:,5]))
-    training_data[:, 5] = stats.zscore((data.loc[:,6]))
-    training_data[:, 6] = stats.zscore((data.loc[:,7]))
-    
-    training_data[:, 7] = stats.zscore((data.loc[:,8]))
-    training_data[:, 8] = stats.zscore((data.loc[:,9]))
-    training_data[:, 9:24] = to_categorical(Label_enc.fit_transform(data[10].values))
-    training_data[:, 24] = stats.zscore((data.loc[:,11]))
-    
-    training_data[:, 25] = stats.zscore((data.loc[:,12]))
-    training_data[:, 26:136] = to_categorical(Label_enc.fit_transform(data[13].values))
-    training_data[:, 136:138] = to_categorical(data[14].values)
-    training_data[:, 139] = to_categorical(Label_enc.fit_transform(data[15].values)).flatten()
-    
-    training_data[:, 140:142] = to_categorical(Label_enc.fit_transform(data[16].values))
-    training_data[:, 142] = stats.zscore((data.loc[:,17]))
-
-    print("loading")
-    
-    training_data[:, 143:174] = to_categorical(Label_enc.fit_transform(
-            [datetime.datetime.strptime(i, '%Y-%m-%d_%H-%M-%S').day for i in data[0].values]
-    ))
-    
-    
-    training_data[:, 174:176] = to_categorical(Label_enc.fit_transform(
-        [datetime.datetime.strptime(i, '%Y-%m-%d_%H-%M-%S').weekday() < 5 for i in data[0].values]
-    ))
-    
-    
-    training_data[:, 176:179] = to_categorical(Label_enc.fit_transform(
-        [datetime.datetime.strptime(i, '%Y-%m-%d_%H-%M-%S').month for i in data[0].values]
-    ))
-    
-    
-    training_data[:, 179:186] = to_categorical(Label_enc.fit_transform(
-        [datetime.datetime.strptime(i, '%Y-%m-%d_%H-%M-%S').weekday() for i in data[0].values]
-    ))
-    
-    training_data[:, 186:210] = to_categorical(Label_enc.fit_transform(
-        [datetime.datetime.strptime(i, '%Y-%m-%d_%H-%M-%S').hour for i in data[0].values]
-    ))
-    
-    labels = data.loc[:,19]
-    print("loaded")
-    return training_data, labels
-
-
-# In[3]:
-
-
-training_data, labels = data()
-
-
-# In[5]:
-
-
-X_train, X_test, y_train, y_test = train_test_split(training_data, labels, test_size=0.25, random_state=11)
-
-
-# In[29]:
-
-
-neigh = KNeighborsRegressor(n_neighbors=4, n_jobs = -1, leaf_size = 400)
-
-
-# In[30]:
-
-
-neigh.fit(X_train, y_train)
-
-
-# In[31]:
-
-
-prediction = neigh.predict(X_test)
-
-
-# In[32]:
-
-
-corr = (y_test.values == prediction)
-t = [e for e in corr if e]
-len(t) / len(corr) * 100
-
-
-# In[33]:
-
-
+from sklearn.model_selection import TimeSeriesSplit, RandomizedSearchCV
+from sklearn.metrics import mean_squared_error
 import matplotlib.pyplot as plt
+from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
 
 
-# In[34]:
+## Encode data
+def get_data(csv):
+    data = pd.read_csv(csv, header=None)
+
+    data[0] = stats.zscore([datetime.datetime.strptime(i, '%Y-%m-%d_%H-%M-%S').timestamp() for i in data[0].values])
+    data[2] = stats.zscore((data.loc[:,2]))
+    data[3] = stats.zscore((data.loc[:,3]))
+    data[4] = stats.zscore((data.loc[:,4]))
+    data[5] = stats.zscore((data.loc[:,5]))
+    data[6] = stats.zscore((data.loc[:,6]))
+    data[7] = stats.zscore((data.loc[:,7]))
+    data[8] = stats.zscore((data.loc[:,8]))
+    data[9] = stats.zscore((data.loc[:,9]))
+    data[11] = stats.zscore((data.loc[:,11]))
+    data[12] = stats.zscore((data.loc[:,12]))
+    data[17] = stats.zscore((data.loc[:,17]))
+    data[18] = stats.zscore((data.loc[:,18]))
+    #data[19] = stats.zscore((data.loc[:,19]))
+
+    data.columns= ['timestamp', 'city', 'av_temp', 'min_temp', 'max_temp', 'pressure',
+                   'humidity', 'visibility', 'wind_speed', 'clouds', 'weather_desc',
+                   'sunrise', 'sundown', 'bike_station_address', 'banking', 'bonus',
+                   'status', 'total_bike_stands', 'available_bike_stands', 'available_bikes']
+
+    dummy = pd.get_dummies(data, prefix=['city', 'weather_desc', 'bike_station_address', 'banking', 'bonus', 'status'],
+                           columns=['city', 'weather_desc', 'bike_station_address', 'banking', 'bonus', 'status'])
+
+    final = dummy.drop(['banking_False', 'status_False', 'available_bike_stands'], axis=1)
+    final = final.fillna(final.mean())
+
+    return final
 
 
-plt.plot(y_test.values[500: 600])
-plt.plot(prediction[500: 600])
+## Split data
+def split_data(full_data):
+
+    # Split data into historic and future data to evaluate our future predictions
+    train_size = int(len(full_data) * 0.75)
+    historic, future = full_data[0:train_size], full_data[train_size:len(full_data)]
+
+    historic_target = historic.available_bikes
+    future_real_target = future.available_bikes
+
+    historic_train = historic.drop(['available_bikes'], axis=1)
+    future_test = future.drop(['available_bikes'], axis=1)
+
+    return historic_train, historic_target, future_test, future_real_target
+
+
+## Cross-validation
+def cross_val(historic_train, historic_target, future_test, future_real_target):
+
+    model = KNeighborsRegressor(n_jobs = 2)
+
+    param_grid = {'n_neighbors':[2, 4, 8, 16]}
+
+    tscv = TimeSeriesSplit(max_train_size=None, n_splits=2)
+    rsearch = RandomizedSearchCV(model, param_grid, n_iter = 10, scoring='neg_mean_absolute_error',
+                                 cv=tscv, verbose=2, random_state=42, n_jobs=-1, return_train_score=True)
+    rsearch.fit(historic_train, historic_target)
+
+    best_params = rsearch.best_params_
+    results = rsearch.cv_results_
+    return results, best_params
+
+
+# Read data
+csv = "../data/transformed_data/merged_data.csv"
+data = get_data(csv)
+historic_train, historic_target, future_test, future_real_target = split_data(data)
+X_train, y_train, X_test, y_test = (historic_train, historic_target, future_test, future_real_target)
+X_train, y_train, X_test, y_test = (historic_train, historic_target, future_test, future_real_target)
+
+
+# Cross validate
+results, best_params = cross_val(historic_train, historic_target, future_test, future_real_target)
+best_params = {'n_neighbors': 16}
+# Plot cross-validation results
+plt.errorbar([2, 4, 8, 16], results["mean_test_score"], results["std_test_score"])
 plt.show()
 
 
-# In[35]:
+# Train final model
+neigh = KNeighborsRegressor(**best_params, n_jobs = 2, leaf_size = 400)
+neigh.fit(X_train, y_train)
+
+prediction = neigh.predict(X_test)
+
+## MAE for test data
+test_mae = mean_absolute_error(y_test, prediction)
+print("Test data MAE: ", test_mae, "\n")
+
+## RMSE
+test_mse = mean_squared_error(y_test, prediction)
+test_rmse = np.sqrt(test_mse)
+print("Test data MSE: ", test_mse)
+print("Test data RMSE: ", test_rmse, "\n")
+
+## R-Squared
+test_r2 = r2_score(y_test, prediction)
+print("Test data R2: ", test_r2, "\n")
 
 
-from sklearn.metrics import mean_squared_error
+## Statistics
+kstest_results_actual_values = stats.kstest(future_real_target, 'norm')
+print("kstest_results_actual_values", kstest_results_actual_values)
+kstest_results_predicted_values = stats.kstest(prediction, 'norm')
+print("kstest_results_predicted_values", kstest_results_predicted_values)
 
+P_VALUE = 0.05
 
-# In[36]:
-
-
-mean_squared_error(y_test.values, prediction)
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
+if kstest_results_predicted_values.pvalue <= P_VALUE or kstest_results_predicted_values.pvalue <= P_VALUE:
+    print(stats.kruskal(future_real_target, prediction))
+    print(stats.mannwhitneyu(future_real_target, prediction))
+else:
+    levene_results = stats.levene(future_real_target, prediction)
+    print("levene_results", levene_results)
+    print(stats.ttest_ind(future_real_target, prediction, equal_var= levene_results.pvalue > P_VALUE))
